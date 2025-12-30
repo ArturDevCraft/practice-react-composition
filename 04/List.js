ccd0b62f9ddccdb30dc55542a70509bd9203fd86
Artur <artur.czerwiak@gmail.com>
2025-12-30 10:52:11 +0100

    Task 04 completed

--- 04/List.js
+++ 04/List.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class List extends React.Component {
-    render() {
-        return <ul>Lista plików</ul>
-    }
+	render() {
+		const { files } = this.props;
+		return (
+			<ul>
+				{files.map((file) => (
+					<li key={Math.random()}>
+						<h3>
+							{file.name} - {file.size / 1024} KB
+						</h3>
+						<p>{file.content}</p>
+					</li>
+				))}
+			</ul>
+		);
+	}
 }
 
 export default List;
--- 04/app.js
+++ 04/app.js
@@ -5,18 +5,43 @@ import File from './File';
 import List from './List';
 
 class App extends React.Component {
-    state = {
-        filesList: [],
-    }
+	state = {
+		filesList: [],
+	};
 
-    render() {
-        return (
-            <section>
-                <File />
-                <List />
-            </section>
-        )
-    }
+	updateFileList = (e) => {
+		const files = e.target.files;
+		console.log(files);
+		for (const file of files) {
+			const reader = new FileReader();
+			let fileContent;
+			reader.onload = () => {
+				fileContent = reader.result;
+				this.setState((state) => {
+					return {
+						filesList: [
+							{ name: file.name, size: file.size, content: fileContent },
+							...state.filesList,
+						],
+					};
+				});
+			};
+			reader.onerror = () => {
+				alert('Error');
+			};
+			reader.readAsText(file);
+		}
+	};
+
+	render() {
+		const { filesList } = this.state;
+		return (
+			<section>
+				<File changeHandler={this.updateFileList} />
+				<List files={filesList} />
+			</section>
+		);
+	}
 }
 
 const root = createRoot(document.querySelector('#root'));
