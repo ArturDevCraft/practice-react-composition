a6c68e9ef18e2f07d301815dafb6df52d7b2cae1
Artur <artur.czerwiak@gmail.com>
2026-01-05 11:30:15 +0100

    Improve list elements key

--- 04/List.js
+++ 04/List.js
@@ -5,14 +5,17 @@ class List extends React.Component {
 		const { files } = this.props;
 		return (
 			<ul>
-				{files.map((file) => (
-					<li key={Math.random()}>
-						<h3>
-							{file.name} - {file.size / 1024} KB
-						</h3>
-						<p>{file.content}</p>
-					</li>
-				))}
+				{files.map((file) => {
+					const fileKey = `${file.name}-${file.size}-${file.lastModified}`;
+					return (
+						<li key={fileKey}>
+							<h3>
+								{file.name} - {file.size / 1024} KB
+							</h3>
+							<p>{file.content}</p>
+						</li>
+					);
+				})}
 			</ul>
 		);
 	}
